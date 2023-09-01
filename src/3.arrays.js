75d005e30fe8ebc73624a74f229731bb501ca8ed
ellisno <143108160+ellisno@users.noreply.github.com>
2023-09-01 12:00:16 -0400

    fixed arrays

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -6,50 +6,65 @@
  * indexoff(arr, 3); should return the index 2
  */
 function indexOf(arr, item) {
-  return ;
+  return arr.indexOf(item);
 }
 
 // Sum: you should be able to sum the items of an array
 // Example: arr = [1,2,3]   =>  6
 function sum(arr) {
-  return ;
+
+   arraySum = 0;
+  for( i=0, n=arr.length; i < n; i++) 
+  { 
+    arraySum += arr[i]; 
+  }
+  return arraySum;
 }
 
 // Filter: Should filter out all instances of a value from an array
 // Example: filtering 2 out of [1,2,3,5,6,2,4,2] should return [1,3,5,6,4]
 function filterOut(arr, item) {
-  return ;
+  const result = arr.filter(x => x !== item)
+
+  return result;
 }
 
 // Append: you should be able to add an item to the end of an array
 function append(arr, item) {
 
-  return ;
+  arr.push(item);
+
+  return arr;
 }
 
 // Truncate: you should be able to remove the last item of an array
 // hint: pop, slice, splice would all work. Pay attention to the return value in the docs
 function truncate(arr) {
-  return ;
+
+  arrayLength = arr.length;
+  newArr = arr.slice(0, (arrayLength - 1))
+  return newArr;
 }
 
 // Concat: you should be able to join together two arrays
 function concat(arr1, arr2) {
-  return ;
+  const arr3 = arr1.concat(arr2);
+  return arr3 ;
 }
 
 // Insert: you should be able to add an item at the specified index of an array
 // hint: use splice and pay attention to the return value
 function insert(arr, item, index) {
-
-  return ;
+  
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 // Square: you should be able to square each number in an array
 // hint: use .map
 // Example: [1,2,4,5] => [1,4,16,25]
 function square(arr) {
-  return ;
+  return arr.map((x)=>Math.pow(x,2));
 }
 
 module.exports = {
