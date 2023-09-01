026a19655c8104c92c08685427591eb05de4b88c
ellisno <143108160+ellisno@users.noreply.github.com>
2023-09-01 12:01:49 -0400

    fixed logical operators

--- src/5.logicalOperators.js
+++ src/5.logicalOperators.js
@@ -1,13 +1,26 @@
-// use the logical operator not if statements
+// use the logical operator not if statements.
 
 // or: should return true if either a or b are truthy. False, if both are false
 function or(a, b) {
-  return ;
+
+  if(a || b ){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
 }
 
-//and: should return true only if both a and b are truthy
+//and: should return true only if both a and b are truthy.
 function and(a, b) {
-  return ;
+  if(a && b ){
+    return true; 
+  }
+  else{
+    return false;
+  }
+  
 }
 
 module.exports = {
