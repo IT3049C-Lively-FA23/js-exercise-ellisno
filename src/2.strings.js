98fcb51be0ca8050f052d2c56a8ebe9aa42bd5ea
ellisno <143108160+ellisno@users.noreply.github.com>
2023-09-01 11:59:40 -0400

    fixed strings

--- src/2.strings.js
+++ src/2.strings.js
@@ -1,30 +1,40 @@
 //reverseString: you should be able to reverse a string
 // Example: "abc" => "cba"
 function reverseString(str) {
-  /**
-   * can be achieved by combining: 
-   *    1. string.prototype.split
-   *    2. array.prototype.reverse
-   *    3. array.prototype.join
-   * 
-   * Can even be done on one line
-   */
-  return ;
+
+
+  splitString = str.split(""); 
+   
+ 
+   
+  reverseArray = splitString.reverse(); 
+
+
+  joinedArray = reverseArray.join(""); 
+
+
+
+ return joinedArray;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+
+ return str.toUpperCase();
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+
+ splittedString = str.split(splitAt);
+
+
+ return splittedString;
 }
 module.exports = {
-  reverseString,
-  capitalize,
-  splitString
+ reverseString,
+ capitalize,
+ splitString
 };
