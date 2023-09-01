35a298f2f133aad25ecfda1c082694a7496c0fc3
ellisno <143108160+ellisno@users.noreply.github.com>
2023-09-01 11:39:20 -0400

    finished flowControl

--- src/6.flowControl.js
+++ src/6.flowControl.js
@@ -1,1 +1,33 @@
 
+/**
+ * write a function that receives a number as its argument;
+ *    if the number is divisible by 3, the function should return `fizz`;
+ *    if the number is divisible by 5, the function should return `buzz`;
+ *    if the number is divisible by 3 and 5, the function should return `fizzbuzz`;
+ *    if the value was provided was a number but doesn't match any of those criteria, return the number as is.
+ *    if no number was provided or if the value provided wasn't a number (hint: typeof), return false
+ */
+function fizzBuzz(num) {
+  
+  if(num % 3 === 0 && num % 5 !== 0){
+    return "fizz"
+  }
+  else if(num % 5 === 0 && num % 3 !== 0){
+    return "buzz"
+  }
+  else if((num % 3 === 0) && (num % 5 === 0)){
+    return "fizzbuzz"
+  }
+  else if( isNaN(num)){
+    return false
+  }
+  else{
+    return num
+  }
+
+}
+  
+
+module.exports = {
+  fizzBuzz
+};
